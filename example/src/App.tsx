9f1f86200ebb1b84970f31f423232c5c8cfee38c
curefatih <curefat@gmail.com>
2021-05-17 17:16:33 +0300

    update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 
-import { BlockWrapper, Block } from 'block-builder'
+import { Block, BlockWrapper, DragableBlock } from 'block-builder'
 import 'block-builder/dist/index.css'
 
 const App = () => {
   return (
-    <div className="" style={{ margin: "0 auto", height: "500px", width: "500px" }}>
+    <div style={{ margin: "0 auto", height: "500px", width: "500px" }}>
       <BlockWrapper
         blockStyle={{
-          height: "100px"
+          height: "100px",
+          border: "1px solid #333"
         }}
       >
         {(value: DOMRect) =>
           <>
             Your components
-            <Block head="Heading" allowInner parentPos={value} style={{ width: 300 }}>
-              Also your components
-            </Block>
+            <DragableBlock head="Heading" allowInner parentPos={value} style={{ width: 300 }}>
+              inner components
+              <Block head="Allowing content" allowInner style={{ width: "100%" }}>components</Block>
+              <Block head="I am not allowing content" style={{ width: "100%" }}>components</Block>
+            </DragableBlock>
+            <DragableBlock head="Heading" allowInner parentPos={value} style={{ width: 300 }}>
+              components
+            </DragableBlock>
           </>
         }
       </BlockWrapper>
